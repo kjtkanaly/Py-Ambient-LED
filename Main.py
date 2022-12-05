67525d2fe87cdd1027569244db62f00b67c19d31
kjtkanaly <kjtkanaly@gmail.com>
2022-12-04 21:19:49 -0500

    Working on sending HSV codes over serial

--- Main.py
+++ Main.py
@@ -1,35 +1,31 @@
-import cv2
 import numpy as np
 import pyautogui
+import colorsys
+import serial
 import time
-from PIL import Image
-from matplotlib import pyplot
+import sys
 
-print("HEllo")
+# Establish the serial connection
+arduino = serial.Serial(port='/dev/cu.usbserial-110', timeout=0)
+time.sleep(1)
 
-screenWidth, screenHeight = pyautogui.size()
-print(str(screenWidth) + ", " + str(screenHeight))
-
-# Move mouse pointer...neat
-#pyautogui.moveTo(100,150)
-
+# Take screenshot of screen
 img = pyautogui.screenshot()
-
-# img.save("Test.jpg")
-
-# Dispaly Screenshot
-# pyplot.imshow(img)
-# pyplot.show()
 
 # Convert screenshot to array
 data = np.asarray(img)
 
-print(type(data))
-print(data.shape)
+# Find the average color
+rgb     = (round(np.mean(data[:,:,0])), round(np.mean(data[:,:,1])), round(np.mean(data[:,:,2])))
+rgb     = (0, 250, 0)
 
-print(round(np.mean(data[:,:,0])))
-print(round(np.mean(data[:,:,1])))
-print(round(np.mean(data[:,:,2])))
+# Convert the RGB to HSV
+hsv = colorsys.rgb_to_hsv(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
 
+print("HSV: " , hsv[0] * 360, hsv[1] * 255, hsv[2] * 255)
 
-
+# Send the HSV values to the arduino
+msg = str("c" + round(hsv[0] * 360)) + " " + str(round(hsv[1] * 255)) + " " + str(round(hsv[2] * 255))
+print(msg)
+# arduino.write(str(hsv[0] * 360).encode)
+#time.sleep(0.1)
--- Main/Main.ino
+++ Main/Main.ino
@@ -12,6 +12,9 @@ int serialData;
 float freq = 0.5;
 float pi = 3.14;
 
+String serialOutput = "";
+String HSV_Code = "";
+
 CRGB leds[numLEDS];
 
 uint8_t hue;
@@ -35,10 +38,66 @@ void setup() {
   FastLED.show();
 }
 
+void UpdateLEDs(String HSV_Code)
+{
+  // Example Code - H125S250V200
+
+  String HueCode = "";
+  String SatCode = "";
+  String ValCode = "";
+
+  int index = 1;
+  while (HSV_Code.substring(index, index+1) != 'S')
+  { 
+    HueCode += HSV_Code.substring(index, index+1);
+    index++;
+  }
+
+  index++;
+
+  while (HSV_Code.substring(index, index+1) != 'V')
+  {
+    SatCode += HSV_Code.substring(index, index+1);
+    index++;
+  }
+
+  index++;
+
+  while (index < HSV_Code.length())
+  {
+    ValCode += HSV_Code.substring(index, index+1);
+    index++;
+  }
+
+  for(int i = 0; i < numLEDS; i++)
+  {
+    leds[i] = CHSV(HueCode.toInt(), SatCode.toInt(), ValCode.toInt());
+  }
+
+  FastLED.show();
+}
+
 // the loop function runs over and over again forever
 void loop() {
-  
+
   while (Serial.available())
+  {
+    serialData = Serial.read();
+    
+    if (char(serialData) != '\n')
+    {
+      serialOutput += char(serialData);
+    }
+
+    else
+    {
+      Serial.println(serialOutput);
+      UpdateLEDs(serialOutput);
+      serialOutput = "";
+    }
+  }
+  
+  /*while (Serial.available())
   {
     serialData = Serial.read();
 
@@ -67,9 +126,20 @@ void loop() {
       Serial.print("Matrix Offline!");
       Serial.print("\n");
     }
+    
+    else if (serialData == 'c')
+    {
+      /*
+      int hue = serialData.substring(0,3).toInt();
+      int sat = serialData.substring(6,9).toInt();
+      int val = serialData.substring(12,15).toInt();
+      
 
-    FastLED.show();
-  }
+      Serial.print(String(hue) + ", " + String(sat) + ", " + String(val));
+
+    }
+
+    FastLED.show();*/
 }
 
 /*Test*/
